91d0a994e3ba642f5cd981e420522667a6097742
BrettHunter <brett@bretthunter.com>
2016-09-17 22:09:03 -0700

    Initial commit

---
+++ lib/ex_congress.ex
@@ -0,0 +1,9 @@
+defmodule ExCongress do
+  @moduledoc """
+  ExCongress is an Elixir wrapper for the [Sunlight Labs Congress API](https://github.com/sunlightlabs/congress). 
+  To see all of the methods available, see:
+  ```
+  h ExCongress.Radar
+  ```
+  """
+end
